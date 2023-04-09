f051bc6a5e7593ba32d72c4d0b50e51b059182c9
Phillip Gallas <gallas.works@outlook.com>
2023-04-09 01:36:00 -0300

    The connection now works

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState({ item: String});
+  const [data, setData] = useState({ item: "" });
 
   useEffect(()=>{
-    const apiUrl = "http://127.0.0.1:8000/connection-test";
+    const apiUrl = "http://127.0.0.1:8000/connection-test-function-based";
 
     fetch(apiUrl)
-      .then((response)=>{response.json()})
-      .then((data)=>setData(data))
+      .then((response)=>response.json())
+      .then(data=>setData(data))
       .catch((error)=>{console.log(error)});
 
   }, []);
+
+
+  /* Perhaps the current problem is due to the fact that I am trying to
+  fetch a Python string, instead of serialized data */
+  // useEffect(()=>{
+  //   console.log(data);
+  // }, [ data ])
   
   return (
-    <div>
+    <>
+      <h1>This is a little weird</h1>
       {data && <h1>{data.item.toString()}</h1>}
-    </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    </>
   );
-}
+} 
 
 export default App;
