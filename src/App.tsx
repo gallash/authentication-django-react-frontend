369638b206947024f58c247419d6a3e43c3c8b78
Phillip Gallas <gallas.works@outlook.com>
2023-04-09 17:57:12 -0300

    React structure further customized. Defined the final structure and will work torward building it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import "./styles.css";
 import { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
   const [data, setData] = useState({ item: "" });
 
   useEffect(()=>{
@@ -21,13 +22,30 @@ function App() {
   // useEffect(()=>{
   //   console.log(data);
   // }, [ data ])
-  
+
+  // This confirms that the API connects
+  // <h1>This is a little weird</h1>
+  //     {data && <h1>{data.item.toString()}</h1>}
+  /* Break the following return into three distinct buttons, one of each auth method */
   return (
     <>
-      <h1>This is a little weird</h1>
-      {data && <h1>{data.item.toString()}</h1>}
+      <div className='presentation-div-container'>
+        <h1 className='presentation-h1'>Authentication Methods</h1>
+        <p className='presentation-p'>This app was built in order to test three ways one could log in into the system.</p>
+        <br/>
+      </div>
+
+      <div className='auth-button-div-container'>
+        <button className='auth-button'>
+          Django's built-in
+        </button>
+        <button className='auth-button'>
+          Token based
+        </button>
+        <button className='auth-button'>
+          OAuth 2.0
+        </button>
+      </div>
     </>
   );
-} 
-
-export default App;
+}
